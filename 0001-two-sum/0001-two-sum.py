a69dc5ef632ea2dc2d406d5d5684907bcb7b239c
Jittania Smith <67732598+jittania@users.noreply.github.com>
2025-04-24 23:13:33 -0700

    Time: 0 ms (100%), Space: 18.8 MB (51.79%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,25 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         """
-        arr of ints (nums), int (target) --> arr of ints (indices)
-
-        Input: nums = [3,3], target = 6
-                         ^
-        { 3: 0 }
-                      
-        Output: [0,1]
-
-        Input: nums = [2,7,11,15], target = 9
-                       ^ 
-        {}
-        Output: [0,1]
-
-        - find the vals in nums that add up to target and return their positions
-        - vals must be distinct (have different positions)
-        - Space
-        - Time
-        - DRY code
-        - Refactor
 
         """
 
