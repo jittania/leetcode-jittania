2bc06132ebcc58eae2540df32ad3434007d12617
Jittania Smith <67732598+jittania@users.noreply.github.com>
2025-04-30 17:32:07 -0700

    Time: 4 ms (70.85%), Space: 19.5 MB (19.63%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,17 +1,33 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+        # Note: this is my favorite Leetcode problem of all time
         """
         Time: O(n)
         Space: O(n)
         """
-        seen = {}
+        # seen = {}
+        # for num in nums:
+        #     if num not in seen:
+        #         seen[num] = 1
+        #     elif num in seen:
+        #         seen[num] += 1
+        #     if seen[num] > (len(nums) // 2):
+        #         return num
+        """
+        Time: O(n)
+        Space: O(1)
+        Note that this solution just utilizes the Boyer-Moore voting algorithm
+        """
+        maj = 0
+        count = 0
+
         for num in nums:
-            if num not in seen:
-                seen[num] = 1
-            elif num in seen:
-                seen[num] += 1
-            if seen[num] > (len(nums) // 2):
-                return num
+            if count == 0:
+                maj = num
+            count += (1 if num == maj else -1)
+        return maj
+
+
 
         
         
