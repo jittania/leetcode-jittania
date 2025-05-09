a2fc2e1a8b2da107a736ba346000e797272e6462
Jittania Smith <67732598+jittania@users.noreply.github.com>
2025-05-08 19:29:34 -0700

    Time: 4 ms (49.01%), Space: 18.6 MB (60.15%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -4,21 +4,16 @@ class Solution:
         Time: O(n)
         Space: O(1)
         """
-        curr_sum = numbers[0] + numbers[-1]
-        left = 0
-        right = len(numbers) - 1
+        left, right = 0, len(numbers) - 1
 
         while left < right:
+            curr_sum = numbers[left] + numbers[right]
             # if curr_sum > target: need to make curr_sum smaller by decreasing right pointer
             if curr_sum > target:
-                curr_sum -= numbers[right]
                 right -= 1
-                curr_sum += numbers[right] 
             # if curr_sum < target: need to make curr_sum bigger by increasing left pointer
             elif curr_sum < target:
-                curr_sum -= numbers[left]
                 left += 1
-                curr_sum += numbers[left]
             # else, they must be equal: return the indices (1 indexed)
             else:
                 return [(left + 1), (right + 1)]
