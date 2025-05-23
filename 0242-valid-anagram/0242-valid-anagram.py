503b8840525ee5c4a4e1c989d3e3ae68acc2b692
Jittania Smith <67732598+jittania@users.noreply.github.com>
2025-05-23 16:34:15 -0700

    Time: 3 ms (98.49%), Space: 17.9 MB (48.86%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -5,6 +5,8 @@ class Solution:
         Time: O(n)
         Space: O(n)
 
+        Note: This approach implicitly handles Unicode characters by using Counter, though any hash map based solution should work as well. 
+
         """
         return (Counter(s) == Counter(t))
         
