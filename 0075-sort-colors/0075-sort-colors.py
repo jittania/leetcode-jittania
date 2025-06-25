10305256257e0853316d77481d6702fe509d9367
Jittania Smith <67732598+jittania@users.noreply.github.com>
2025-06-24 17:46:21 -0700

    Time: 0 ms (100%), Space: 17.9 MB (33.73%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,21 +1,41 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
-        Time: O(2n) -> Two-pass solution
+        Time: O(2n) -> Two-pass solution w/ Bucket Sort
         Space: O(1)
         """
-        # Make a count hash map (first pass)
-        counts = collections.Counter(nums)
-        start = 0
+        # # Make a count hash map
+        # buckets = collections.Counter(nums)
+        # start = 0
 
-        # Go over nums and reassign values using hash map
-        for color in range(3):
-            for i in range(counts[color]):
-                nums[i + start] = color
-                temp = i
-            start += counts[color]
+        # # Go over nums and reassign values using hash map
+        # for color in range(3):
+        #     for i in range(buckets[color]):
+        #         nums[i + start] = color
+        #         temp = i
+        #     start += buckets[color]
 
         """
-        Time: O(n) -> One-pass solution
+        Time: O(n) -> One-pass solution w/ partitioning
         Space: O(1)
         """
+        # l = boundary for 0s (everything before l is 0)
+        # r = boundary for 2s (everything after r is 2)
+        # i = current index being examined
+        l, r = 0, len(nums) - 1
+        i = 0
+
+        # If we find a 0 or a 2 at i, swap with left or right respectively 
+        # Only increment i when you're confident you've placed a 0 or 1 in its final place
+        while i <= r:
+            if nums[i] == 0:
+                nums[l], nums[i] = nums[i], nums[l]
+                l += 1
+                i += 1
+            elif nums[i] == 2:
+                nums[r], nums[i] = nums[i], nums[r]
+                r -= 1
+            else:
+                i += 1
+                
+
